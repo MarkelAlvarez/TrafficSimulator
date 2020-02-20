ef31aeea826dd901c7dc58c8e29af47fde556fc4
Markel <markelalvarez99@gmail.com>
2020-02-20 18:35:21 +0100

    Completed some methods and constructor fixed

--- src/simulator/model/Vehicle.java
+++ src/simulator/model/Vehicle.java
@@ -10,18 +10,19 @@ public class Vehicle extends SimulatedObject {
 	private int velocMaxima;
 	private int velocActual;
 	private VehicleStatus estado;
-	private Road carretera;
-	private int localizacion;
+	private Road carretera = null;
+	private int localizacion = 0;
 	private int gradoCont;
 	private int contTotal;
 	private int distTotRec;
 	
-	Vehicle(String id, int maxSpeed, int contClass,
-			List<Junction> itinerary) {
+	Vehicle(String id, int maxSpeed, int contClass, List<Junction> itinerary) {
+		
 		super(id);
-		if(velocMaxima<0) throw new IllegalArgumentException("La velocidad maxima tiene que ser un numero positivo.");
-		if(contClass < 0 || contClass > 10) throw new IllegalArgumentException ("El grado de contaminacion tiene que tener un valor entre 0 y 10.");
-		if(itinerary.leght < 2) throw new IllegalArgumentException ("El itinerario tiene que tener al menos dos elementos.");
+		
+		if(maxSpeed < 0) throw new IllegalArgumentException("La velocidad maxima tiene que ser un numero positivo.");
+		if(contClass < 0 || contClass > 10) throw new IllegalArgumentException("El grado de contaminacion tiene que tener un valor entre 0 y 10.");
+		if(itinerary.length() < 2) throw new IllegalArgumentException("El itinerario tiene que tener al menos dos elementos.");
 	
 		velocMaxima = maxSpeed;
 		gradoCont = contClass;
@@ -40,21 +41,36 @@ public class Vehicle extends SimulatedObject {
 		return null;
 	}
 	
+	void moveToNextRoad() {
+		
+	}
+	
+	/*GETS & SETS*/
+	
 	void setSpeed(int s) {
 		
+		if (s < 0)
+		{
+			throw new IllegalArgumentException("La velocidad debe ser positiva.");
+		}
+		
+		if (s <= velocMaxima)
+		{
+			velocActual = s;
+		}
+		else
+		{
+			velocActual = velocMaxima;
+		}
 	}
 	
 	void setContaminationClass(int c) {
 		
-	}
-	
-	void moveToNextRoad() {
+		if (gradoCont < 0 || gradoCont > 10)
+		{
+			throw new IllegalArgumentException("El valor de contaminación debe se entre 0 y 10 (ambos inclusive).");
+		}
 		
+		gradoCont = c;
 	}
-
-	public JSONObject report() {
-		
-	}
-	
-	
 }
