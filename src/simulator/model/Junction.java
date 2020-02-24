15023f6e84e25372b1981ffc1b064023622067eb
nuxa17 <56154305+nuxa17@users.noreply.github.com>
2020-02-24 12:16:20 +0100

    los cruce y estrategias que no sabemos donde meter y eso

---
+++ src/simulator/model/Junction.java
@@ -0,0 +1,38 @@
+package simulator.model;
+
+import java.util.*;
+
+import org.json.JSONObject;
+
+public class Junction extends SimulatedObject {
+
+	private List<Road> listaEntrantes;
+	private Map<Junction,Road> mapaSalientes;
+	private List<List<Vehicle >> listaColas;
+	private Map<Road,List<Vehicle>> carretera_cola;
+	private int indiceVerde;
+	private int pasoSemaforo;
+	private LightSwitchingStrategy estratSem;
+	private DequeuingStrategy estratCola;
+	
+	
+	protected Junction(String id, LightSwitchStrategy lsStrategy, DequeingStrategy
+			dqStrategy, int xCoor, int yCoor) {
+		
+		super(id);
+		// ...
+	}
+
+	@Override
+	void advance(int time) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public JSONObject report() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/simulator/model/RoundRobinStrategy.java
@@ -0,0 +1,18 @@
+package simulator.model;
+
+import java.util.*;
+
+public class RoundRobinStrategy implements LightSwitchingStrategy {
+
+	public RoundRobinStrategy() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int chooseNextGreen(List<Road> roads, List<List<Vehicle>> qs, int currGreen, int lastSwitchingTime,
+			int currTime) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
