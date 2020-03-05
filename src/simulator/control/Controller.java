1c4fc46985fe0e3d0c7dad2be73ff0126641c69e
nuxa17 <56154305+nuxa17@users.noreply.github.com>
2020-03-05 18:48:44 +0100

    controller cosas

--- src/simulator/control/Controller.java
+++ src/simulator/control/Controller.java
@@ -1,5 +1,30 @@
 package simulator.control;
 
+import simulator.factories.*;
+import simulator.model.*;
+
 public class Controller {
 
+	private TrafficSimulator sim;
+	private Factory<Event> eventosFact;
+	
+	public Controller(TrafficSimulator sim, Factory<Event> eventsFactory)
+	{
+	// TODO complete
+	}
+	
+	public void loadEvents(InputStream in) {
+		//TODO: todo
+		JSONObject jo = new JSONObject(new JSONTokener(in));
+	}
+	
+	public void run(int n, OutputStream out) {
+		//TODO: todo
+
+	}
+	
+	public void reset() {
+		//TODO: todo
+
+	}
 }
