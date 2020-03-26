717189abd77f667a16584c8d3e74922280cec533
Markel <markelalvarez99@gmail.com>
2020-03-26 13:07:04 +0100

    Tables created

--- src/simulator/view/MainWindow.java
+++ src/simulator/view/MainWindow.java
@@ -34,20 +34,33 @@ public class MainWindow extends JFrame {
 		JPanel mapsPanel = new JPanel();
 		mapsPanel.setLayout(new BoxLayout(mapsPanel, BoxLayout.Y_AXIS));
 		viewsPanel.add(mapsPanel);
-		// tables
 		
+		// tables
 		JPanel eventsView = createViewPanel( new JTable(new EventsTableModel(_ctrl)), "Events");
 		eventsView.setPreferredSize(new Dimension(500, 200));
 		tablesPanel.add(eventsView);
-		// TODO add other tables
-		// ...
+		
+		JPanel vehiclesView = createViewPanel( new JTable(new VehiclesTableModel(_ctrl)), "Vehicles");
+		vehiclesView.setPreferredSize(new Dimension(500, 200));
+		tablesPanel.add(vehiclesView);
+		
+		JPanel roadsView = createViewPanel( new JTable(new RoadsTableModel(_ctrl)), "Roads");
+		roadsView.setPreferredSize(new Dimension(500, 200));
+		tablesPanel.add(roadsView);
+		
+		JPanel junctionView = createViewPanel( new JTable(new JunctionsTableModel(_ctrl)), "Junctions");
+		junctionView.setPreferredSize(new Dimension(500, 200));
+		tablesPanel.add(junctionView);
 		
 		// maps
 		JPanel mapView = createViewPanel(new MapComponent(_ctrl), "Map");
 		mapView.setPreferredSize(new Dimension(500, 400));
 		mapsPanel.add(mapView);
+		
+		JPanel roadView = createViewPanel(new MapComponent(_ctrl), "Map by Road");
+		roadView.setPreferredSize(new Dimension(500, 400));
+		mapsPanel.add(roadView);
 		// TODO add a map for MapByRoadComponent
-		// ...
 		
 		this.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
 		this.pack();
