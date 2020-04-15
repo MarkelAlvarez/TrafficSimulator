f4a987cd98e65275298a045bdc3ec9d67faf0452
Markel <markelalvarez99@gmail.com>
2020-04-15 12:11:55 +0200

    ControlPanel elements added correctly

--- src/simulator/view/ControlPanel.java
+++ src/simulator/view/ControlPanel.java
@@ -2,6 +2,7 @@ package simulator.view;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.util.*;
 import java.util.List;
@@ -18,7 +19,7 @@ import simulator.model.Event;
 
 public class ControlPanel extends JPanel implements TrafficSimObserver {
 
-	private JToolBar toolbar;
+	private JToolBar barraHerramientas;
 	private JButton botonFichero;
 	private JButton botonCO2;
 	private JButton botonWeather;
@@ -28,91 +29,183 @@ public class ControlPanel extends JPanel implements TrafficSimObserver {
 	
 	private JLabel textoTicks;
 	private JSpinner ticks;
+	private int _ticks;
 	
 	private JFileChooser selectorFichero;
 	private ChangeCO2ClassDialog changeCO2;
 	private ChangeWeatherDialog changeWeather;
 	private JDialog exit;
 	
-	private JSeparator separador1;
-	private JSeparator separador2;
-	private JSeparator separador3;
-	private JSeparator separador4;
-	
-	private Controller _ctrl;
-	private boolean _stopped = false;
+	private Controller ctrl;
+	private boolean _stopped;
 	
 	public ControlPanel(Controller _ctrl) {
 		
-		this._ctrl = _ctrl;
+		ctrl = _ctrl;
+		_stopped = true;
+		initGUI();
+		ctrl.addObserver(this);	
+	}
+	
+	private void initGUI() {
 		
-		toolbar = new JToolBar();
-		this.add(toolbar);
+		barraHerramientas = new JToolBar();
+		this.setLayout(new BorderLayout());
+		this.add(barraHerramientas, BorderLayout.PAGE_START);
 
-		separador1 = new JSeparator(SwingConstants.VERTICAL);
-		toolbar.add(separador1);
+		barraHerramientas.addSeparator();
+		botonCargaFichero();
+		barraHerramientas.addSeparator();
+		botonCO2();
+		botonWeather();	
+		barraHerramientas.addSeparator();
+		botonRun();
+		botonStop();
+		spinnerTicks();
+		barraHerramientas.add(Box.createHorizontalGlue());
+		barraHerramientas.addSeparator();
+		botonExit();
+	}
 
-		botonFichero = new JButton();
-		botonFichero.setActionCommand("cargar");
-		botonFichero.setIcon(new ImageIcon("./resources/icons/open.png"));
-		toolbar.add(botonFichero);
-		
-		separador2 = new JSeparator(SwingConstants.VERTICAL);
-		toolbar.add(separador2);
-		
-		botonCO2 = new JButton();
-		botonCO2.setActionCommand("co2");
-		botonCO2.setIcon(new ImageIcon("./resources/icons/co2class.png"));
-		toolbar.add(botonCO2);
-		
-		botonWeather = new JButton();
-		botonWeather.setActionCommand("weather");
-		botonWeather.setIcon(new ImageIcon("./resources/icons/weather.png"));
-		toolbar.add(botonWeather);
-		
-		separador3 = new JSeparator(SwingConstants.VERTICAL);
-		toolbar.add(separador3);
-		
-		run = new JButton();
-		run.setActionCommand("run");
-		run.setIcon(new ImageIcon("./resources/icons/run.png"));
-		toolbar.add(run);
-		
-		stop = new JButton();
-		stop.setActionCommand("stop");
-		stop.setIcon(new ImageIcon("./resources/icons/stop.png"));
-		toolbar.add(stop);
-		
-		textoTicks = new JLabel("Ticks: ", JLabel.CENTER);
-		ticks = new JSpinner(new SpinnerNumberModel(10, 1, 300, 1));
-		// TODO: hay que cambiar aqui el valor de los ticks?
-		ticks.addChangeListener(new ChangeListener() {
-			@Override
-			public void stateChanged(ChangeEvent e) {
-				// Ponemos el valor del JSpinner en el JTextField
-				Main.setTicks((Integer) ticks.getValue());
-			}
-		});
-		toolbar.add(ticks);
-		
-		toolbar.add(Box.createGlue());
-
-		separador4 = new JSeparator(SwingConstants.VERTICAL);
-		toolbar.add(separador4);
-		
-		botonExit = new JButton();
-		botonExit.setActionCommand("exit");
-		botonExit.setIcon(new ImageIcon("./resources/icons/exit.png"));
-		toolbar.add(botonExit);
+	private void botonCargaFichero() {
 		
 		selectorFichero = new JFileChooser();
+		selectorFichero.setDialogTitle("Carga de fichero de datos");
 		selectorFichero.setCurrentDirectory(new File("./resources/examples/"));
 		selectorFichero.setMultiSelectionEnabled(false);
 		selectorFichero.setFileFilter(new FileNameExtensionFilter("JavaScript Object Notation (JSON)", "json"));
-		toolbar.add(selectorFichero);
+		
+		botonFichero = new JButton();
+		//botonFichero.setActionCommand("cargar");
+		botonFichero.setToolTipText("Carga los archivos de datos a la aplicación");
+		botonFichero.setIcon(new ImageIcon("./resources/icons/open.png"));
+		botonFichero.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				int ret = selectorFichero.showOpenDialog(this);
+				if (ret == JFileChooser.APPROVE_OPTION)
+				{
+					//TODO: cargar los datos en el programa	
+				}
+				else
+				{
+					JOptionPane.showMessageDialog(this, "Se ha pulsado cancelar o ha ocurrido un error.");
+				}			
+			}
+		});
+		
+		barraHerramientas.add(botonFichero);
+	}
+	
+	private void botonCO2() {
+		
+		botonCO2 = new JButton();
+		//botonCO2.setActionCommand("co2");
+		botonCO2.setIcon(new ImageIcon("./resources/icons/co2class.png"));
+		botonCO2.setToolTipText("Modificar información de la contaminación");
+		botonFichero.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				//TODO: ventana emergente				
+			}
+		});
+		
+		barraHerramientas.add(botonCO2);
+	}
+	
+	private void botonWeather() {
+		
+		botonWeather = new JButton();
+		//botonWeather.setActionCommand("weather");
+		botonWeather.setToolTipText("Modificar información del clima");
+		botonWeather.setIcon(new ImageIcon("./resources/icons/weather.png"));
+		botonWeather.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				//TODO: ventana emergente				
+			}
+		});
+		barraHerramientas.add(botonWeather);
+	}
+	
+
+	private void botonRun() {
+		
+		run = new JButton();
+		//run.setActionCommand("run");
+		run.setToolTipText("Iniciar simulación");
+		run.setIcon(new ImageIcon("./resources/icons/run.png"));
+		run.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				enableToolBar(false);
+				_stopped = false; //TODO: comprobar 
+				run_sim((Integer) ticks.getValue());			
+			}
+		});
+		
+		barraHerramientas.add(run);
 	}
 
-	public void actionPerformed(ActionEvent e) {
+	private void botonStop() {
+
+		stop = new JButton();
+		//stop.setActionCommand("stop");
+		stop.setToolTipText("Parar simulación");
+		stop.setIcon(new ImageIcon("./resources/icons/stop.png"));
+		stop.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				stop();		
+			}
+		});
+		barraHerramientas.add(stop);
+	}
+	
+	private void spinnerTicks() {
+		
+		textoTicks = new JLabel("Ticks: ", JLabel.CENTER);
+		
+		ticks = new JSpinner(new SpinnerNumberModel(10, 1, 300, 1));
+		ticks.setMinimumSize(new Dimension(80, 30));
+		ticks.setMaximumSize(new Dimension(200, 30));
+		ticks.setPreferredSize(new Dimension(80, 30));
+		ticks.addChangeListener(new ChangeListener() {
+			@Override
+			public void stateChanged(ChangeEvent e) {
+
+				_ticks = Integer.valueOf(ticks.getValue().toString());
+			}
+		});
+		
+		barraHerramientas.add(textoTicks);
+		barraHerramientas.add(ticks);
+		
+	}
+	
+	private void botonExit() {
+
+		botonExit = new JButton();
+		//botonExit.setActionCommand("exit");
+		botonExit.setAlignmentX(RIGHT_ALIGNMENT);
+		botonExit.setToolTipText("Cerrar aplicación");
+		botonExit.setIcon(new ImageIcon("./resources/icons/exit.png"));
+		botonExit.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				int ret = JOptionPane.showConfirmDialog(null, "¿Desea cerrar el programa?", "Salir", JOptionPane.YES_NO_OPTION);
+				
+				if (ret == 0)
+				{
+					System.exit(0);
+				}
+			}
+		});
+		
+		barraHerramientas.add(botonExit);
+	}
+
+	/*public void actionPerformed(ActionEvent e) {
 		
 		if (e.getActionCommand().equals("cargar"))
 		{
@@ -136,7 +229,7 @@ public class ControlPanel extends JPanel implements TrafficSimObserver {
 		}
 		else if (e.getActionCommand().equals("run"))
 		{
-			_stopped = false; //TODO: comprobar 
+			_stopped = false;
 			run_sim((Integer) ticks.getValue());
 		}
 		else if (e.getActionCommand().equals("stop"))
@@ -152,16 +245,17 @@ public class ControlPanel extends JPanel implements TrafficSimObserver {
 				System.exit(0);
 			}
 		}
-	}
+	}*/
 	
 	private void run_sim(int n) {
 		
 		if (n > 0 && !_stopped)
 		{
 			try {
-				_ctrl.run(1, null);
+				ctrl.run(1, null);
 			} catch (Exception e ) {
-				// TODO show error message
+				JOptionPane.showMessageDialog(null, "Se ha producido un error");
+				enableToolBar(true);
 				_stopped = true;
 				return;
 			}
@@ -180,6 +274,17 @@ public class ControlPanel extends JPanel implements TrafficSimObserver {
 		}
 	}
 	
+	private void enableToolBar(boolean b) {
+		
+		botonFichero.setEnabled(b);
+		botonCO2.setEnabled(b);
+		botonWeather.setEnabled(b);
+		run.setEnabled(b);
+		stop.setEnabled(b);
+		botonExit.setEnabled(b);
+		ticks.setEnabled(b);
+	}
+
 	private void stop() {
 		
 		_stopped = true ;
