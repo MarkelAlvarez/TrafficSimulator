6315863d9df529518af721639220b10e8c93cbc4
Markel <markelalvarez99@gmail.com>
2020-04-15 12:37:58 +0200

    StatusBar implemented

--- src/simulator/view/StatusBar.java
+++ src/simulator/view/StatusBar.java
@@ -3,6 +3,7 @@ package simulator.view;
 import java.util.List;
 
 import javax.swing.*;
+import java.awt.*;
 
 import simulator.control.Controller;
 import simulator.launcher.Main;
@@ -13,59 +14,65 @@ import simulator.model.TrafficSimObserver;
 public class StatusBar extends JPanel implements TrafficSimObserver {
 
 	private JLabel tiempo;
-	private JSeparator separador;
 	private JLabel eventoAñadido;
 	
-	private Controller _ctrl;
+	private JLabel currentTime;
+	private JLabel currentEvent;
+	
+	private Controller ctrl;
 	
 	public StatusBar(Controller _ctrl) {
 		
-		this._ctrl = _ctrl;
+		ctrl = _ctrl;
+		initGUI();
+		ctrl.addObserver(this);
+	}
+
+	private void initGUI() {
+		
+		this.setLayout(new FlowLayout(FlowLayout.LEFT));
+		this.setBorder(BorderFactory.createBevelBorder(1));
+		
+		tiempo = new JLabel("Time: " + currentTime, JLabel.LEFT);
+		this.add(tiempo);
+		
+		this.add(new JSeparator(SwingConstants.VERTICAL));
+		
+		eventoAñadido = new JLabel("Event added: " + currentEvent, JLabel.LEFT);
+		this.add(eventoAñadido);
 	}
 
 	@Override
 	public void onAdvanceStart(RoadMap map, List<Event> events, int time) {
 		
-		tiempo = new JLabel("Tiempo: " + time, JLabel.LEFT);
-		separador = new JSeparator(SwingConstants.VERTICAL);
-		//eventoAñadido = new JLabel("Event Added: (" + e.toString() + ")", JLabel.LEFT);
+		currentTime.setText(String.valueOf(time));
 	}
 
 	@Override
 	public void onAdvanceEnd(RoadMap map, List<Event> events, int time) {
-		
-		tiempo = new JLabel("Tiempo: " + time, JLabel.LEFT);
-		separador = new JSeparator(SwingConstants.VERTICAL);
-		//eventoAñadido = new JLabel("Event Added: (" + e.toString() + ")", JLabel.LEFT);
 	}
 
 	@Override
 	public void onEventAdded(RoadMap map, List<Event> events, Event e, int time) {
 		
-		tiempo = new JLabel("Tiempo: " + time, JLabel.LEFT);
-		separador = new JSeparator(SwingConstants.VERTICAL);
-		//eventoAñadido = new JLabel("Event Added: (" + e.toString() + ")", JLabel.LEFT);
+		currentEvent.setText(String.valueOf(events.get(time)));
 	}
 
 	@Override
 	public void onReset(RoadMap map, List<Event> events, int time) {
 		
-		tiempo = new JLabel("Tiempo: " + time, JLabel.LEFT);
-		separador = new JSeparator(SwingConstants.VERTICAL);
-		//eventoAñadido = new JLabel("Event Added: (" + e.toString() + ")", JLabel.LEFT);
+		currentTime.setText(String.valueOf(0));
+		currentEvent.setText(String.valueOf(events.get(time)));
 	}
 
 	@Override
 	public void onRegister(RoadMap map, List<Event> events, int time) {
 	
-		tiempo = new JLabel("Tiempo: " + time, JLabel.LEFT);
-		separador = new JSeparator(SwingConstants.VERTICAL);
-		//eventoAñadido = new JLabel("Event Added: (" + e.toString() + ")", JLabel.LEFT);
+		currentTime.setText(String.valueOf(time));
+		currentEvent.setText(String.valueOf(events.get(time)));
 	}
 
 	@Override
 	public void onError(String err) {
-
-		// TODO Auto-generated method stub
 	}
 }
