fe3b0be5c0109b213db7cccb2d0068269dad95b1
MarkelAlvarez <markelalvarez99@gmail.com>
2020-04-29 15:49:14 +0200

    Road table implemented but not working properly

--- src/simulator/view/RoadsTableModel.java
+++ src/simulator/view/RoadsTableModel.java
@@ -1,81 +1,133 @@
 package simulator.view;
 
+import java.util.ArrayList;
 import java.util.List;
-
 import javax.swing.table.AbstractTableModel;
 
 import simulator.control.Controller;
-import simulator.model.Event;
-import simulator.model.RoadMap;
-import simulator.model.TrafficSimObserver;
+import simulator.model.*;
 
 public class RoadsTableModel extends AbstractTableModel implements TrafficSimObserver {
 
+	private List<Road> _roads;
+	private String[] _colNames = { "ID", "Longitud", "Weather", "Max Speed", "Act. Speed", "Total CO2", "CO2 Limit" };
+	private Controller ctrl;
+
 	public RoadsTableModel(Controller _ctrl) {
 		
-		// TODO Auto-generated constructor stub
+		ctrl = _ctrl;
+		_roads = new ArrayList<Road>();
+		ctrl.addObserver(this);
 	}
 
-	@Override
-	public int getRowCount() {
+	public void update() {
+		// observar que si no refresco la tabla no se carga
+		// La tabla es la represantación visual de una estructura de datos,
+		// en este caso de un ArrayList, hay que notificar los cambios.
+		// We need to notify changes, otherwise the table does not refresh.
+		fireTableDataChanged();	
+	}
+	
+	public void setRoadsList(List<Road> roads) {
 		
-		// TODO listaEventos.size()
-		return 0;
+		_roads = roads;
+		update();
 	}
 
 	@Override
+	public boolean isCellEditable(int row, int column) {
+		
+		return false;
+	}
+
+	//si no pongo esto no coge el nombre de las columnas
+	//this is for the column header
+	@Override
+	public String getColumnName(int col) {
+		
+		return _colNames[col];
+	}
+
+	@Override
+	// método obligatorio, probad a quitarlo, no compila
+	// this is for the number of columns
 	public int getColumnCount() {
 		
-		// TODO columnas.lenght()
-		return 0;
-	}
-
-	public int getColumnName()
-	{
-		//TODO
-		return 0;
+		return _colNames.length;
 	}
 
 	@Override
-	public Object getValueAt(int rowIndex, int columnIndex) {
+	// método obligatorio
+	// the number of row, like those in the events list
+	public int getRowCount() {
 		
-		// TODO Auto-generated method stub
-		return null;
+		return _roads == null ? 0 : _roads.size();
 	}
 
+	@Override
+	// método obligatorio
+	// así es como se va a cargar la tabla desde el ArrayList
+	// el índice del arrayList es el número de fila pq en este ejemplo
+	// quiero enumerarlos.
+	// returns the value of a particular cell 
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		
+		Object s = null;
+		//TODO: los datos aparecen mal
+		switch (columnIndex)
+		{
+			case 0:
+				s = _roads.get(rowIndex).getId();
+				break;
+			case 1:
+				s = _roads.get(rowIndex).getLongitud();
+				break;
+			case 2:
+				s = _roads.get(rowIndex).getCondMet();
+			case 3:
+				s = _roads.get(rowIndex).getVelocMaxima();
+			case 4:
+				s = _roads.get(rowIndex).getLimiteActual();
+			case 5:
+				s = _roads.get(rowIndex).getContTotal();
+			case 6:
+				s = _roads.get(rowIndex).getLimiteCont();
+			default:
+				break;
+		}
+		
+		return s;
+	}
+	
 	@Override
 	public void onAdvanceStart(RoadMap map, List<Event> events, int time) {
 		
-		// TODO Auto-generated method stub
+		setRoadsList(map.getRoads());
 	}
 
 	@Override
 	public void onAdvanceEnd(RoadMap map, List<Event> events, int time) {
 		
-		// TODO Auto-generated method stub
+		setRoadsList(map.getRoads());
 	}
 
 	@Override
 	public void onEventAdded(RoadMap map, List<Event> events, Event e, int time) {
 		
-		// TODO Auto-generated method stub
+		setRoadsList(map.getRoads());
 	}
 
 	@Override
 	public void onReset(RoadMap map, List<Event> events, int time) {
 		
-		// TODO Auto-generated method stub
+		setRoadsList(map.getRoads());
 	}
 
 	@Override
 	public void onRegister(RoadMap map, List<Event> events, int time) {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	public void onError(String err) {
-		
-		// TODO Auto-generated method stub
 	}
 }
